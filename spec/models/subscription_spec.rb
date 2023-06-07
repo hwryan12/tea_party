1b286b25f849afef94cc8da3157f9c065b6e46e9
Harrison Ryan <hwryan12@gmail.com>
2023-06-07 08:07:09 -0500

    feature: Add models relationships and validations

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  it { should validate_presence_of(:title) }
-  it { should validate_presence_of(:price) }
-  it { should validate_presence_of(:status) }
-  it { should have_many(:customers) }
-  it { should have_many(:teas) }
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:price) }
+    it { should validate_presence_of(:status) }
+  end
+
+  describe 'relationships' do
+    it { should belong_to(:customer) }
+    it { should have_many(:subscription_teas) }
+    it { should have_many(:teas).through(:subscription_teas) }
+  end
 end
